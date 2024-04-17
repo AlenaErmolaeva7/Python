a380ac520164f6101dd873c956e7947679d90e2e
Алена Ермолаева <alenaermolaeva755@gmail.com>
2024-04-17 17:22:53 +0300

    Delete lesson_3_task_3.py

--- lesson_3_task_3.py
+++
@@ -1,6 +0,0 @@
-from address import Address
-from mailing import Mailing
-letter = Mailing(Address("123456", "Москва", "Арбат", "12", "1"),
-                        Address("789000", "Новосибирск", "Ленина", "24", "15"),
-                        "516", "456210987654")
-print(f'Отправление {letter.track} из {letter.from_address} в {letter.to_address}. Стоимость {letter.cost} рублей.')
